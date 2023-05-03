cfce96ab7754100418ff74bbe1232be4de8ea458
Nadith Dhanula <116380737+nadith-pro@users.noreply.github.com>
2023-05-03 18:31:28 +0530

    Create Mike And Dave Need Wedding Dates .js

---
+++ plugins/Movies/English/Mike And Dave Need Wedding Dates .js
@@ -0,0 +1,35 @@
+const { cmd, commands } = require('../../../commands')
+
+cmd({
+    pattern: "mikeanddaveneedweddingdates",
+    filename: __filename
+},
+    async (conn, mek, q, { reply, config, isSUB, from, sendjid, isowner, isMe, pushname, botNumber, senderNumber, sender, isGroup, args, isCmd, command, body, quoted }) => {
+        try {
+
+            if (!isSUB) return
+
+            conn.sendMessage(from, { react: { text: config.RTYPE3, key: mek.key } })
+
+            for (let i = 0; i < sendjid.length; i++) {
+
+                await conn.sendMessage(sendjid[i], {
+                    caption: "Mike And Dave Need Wedding Dates | 2016 | 18+ | English | Movie | Sinhala Subtitles | TV Zone | " + config.FOOTER,
+                    footer: config.FOOTER,
+                    image: { url: "https://telegra.ph/file/88b85bb7dea472be102ce.jpg" }
+                })
+
+                conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+                await conn.sendMessage(sendjid[i], {
+                    document: { url: config.DBLINK + config.ENMVLINK + 'Mike_And_Dave_Need_Wedding_Dates_2016_@nadithpro.mkv' },
+                    mimetype: config.MKVTYPE,
+                    fileName: '@nadithpro Mike And Dave Need Wedding Dates 2016 18+ .mkv'
+                })
+            } conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+
+        } catch (e) {
+            reply('ERROR !!')
+            console.log(e)
+        }
+    })
