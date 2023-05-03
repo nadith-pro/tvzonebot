a91ffb8c07957eeae55657b96b2a67b85f80c5ae
Nadith Dhanula <116380737+nadith-pro@users.noreply.github.com>
2023-05-03 18:31:21 +0530

    Create Max .js

---
+++ plugins/Movies/English/Max .js
@@ -0,0 +1,35 @@
+const { cmd, commands } = require('../../../commands')
+
+cmd({
+    pattern: "max",
+    filename: __filename
+},
+    async (conn, mek, q, { reply, config, isSUB, from, sendjid, isowner, isMe, pushname, botNumber, senderNumber, sender, isGroup, args, isCmd, command, body, quoted }) => {
+        try {
+
+            if (!isSUB) return
+
+            conn.sendMessage(from, { react: { text: config.RTYPE3, key: mek.key } })
+
+            for (let i = 0; i < sendjid.length; i++) {
+
+                await conn.sendMessage(sendjid[i], {
+                    caption: "Max White House Hero | 2017 | English | Movie | Sinhala Subtitles | TV Zone | " + config.FOOTER,
+                    footer: config.FOOTER,
+                    image: { url: "https://telegra.ph/file/25113ce641ec05324b12a.jpg" }
+                })
+
+                conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+                await conn.sendMessage(sendjid[i], {
+                    document: { url: config.DBLINK + config.ENMVLINK + 'Max_White_House_Hero_2017_@nadithpro.mkv' },
+                    mimetype: config.MKVTYPE,
+                    fileName: '@nadithpro Max White House Hero 2017 .mkv'
+                })
+            } conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+
+        } catch (e) {
+            reply('ERROR !!')
+            console.log(e)
+        }
+    })
