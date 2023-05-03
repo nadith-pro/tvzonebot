445d8cf231297d57e819f28182d4f6a9d0c46bde
Nadith Dhanula <116380737+nadith-pro@users.noreply.github.com>
2023-05-03 18:31:18 +0530

    Create Man Of Steel .js

---
+++ plugins/Movies/English/Man Of Steel .js
@@ -0,0 +1,35 @@
+const { cmd, commands } = require('../../../commands')
+
+cmd({
+    pattern: "manofsteel",
+    filename: __filename
+},
+    async (conn, mek, q, { reply, config, isSUB, from, sendjid, isowner, isMe, pushname, botNumber, senderNumber, sender, isGroup, args, isCmd, command, body, quoted }) => {
+        try {
+
+            if (!isSUB) return
+
+            conn.sendMessage(from, { react: { text: config.RTYPE3, key: mek.key } })
+
+            for (let i = 0; i < sendjid.length; i++) {
+
+                await conn.sendMessage(sendjid[i], {
+                    caption: "Man Of Steel | 2013 | English | Movie | Sinhala Subtitles | TV Zone | " + config.FOOTER,
+                    footer: config.FOOTER,
+                    image: { url: "https://telegra.ph/file/82b22aeb646bdec5ad5bc.jpg" }
+                })
+
+                conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+                await conn.sendMessage(sendjid[i], {
+                    document: { url: config.DBLINK + config.ENMVLINK + 'Man_Of_Steel_2013_@nadithpro.mkv' },
+                    mimetype: config.MKVTYPE,
+                    fileName: '@nadithpro Man Of Steel 2013 .mkv'
+                })
+            } conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+
+        } catch (e) {
+            reply('ERROR !!')
+            console.log(e)
+        }
+    })
