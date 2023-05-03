e33fd8c7afae118b728d9f0f0d93e740c0f13674
Nadith Dhanula <116380737+nadith-pro@users.noreply.github.com>
2023-05-03 18:31:10 +0530

    Create Jungle Cruise .js

---
+++ plugins/Movies/English/Jungle Cruise .js
@@ -0,0 +1,35 @@
+const { cmd, commands } = require('../../../commands')
+
+cmd({
+    pattern: "junglecruise",
+    filename: __filename
+},
+    async (conn, mek, q, { reply, config, isSUB, from, sendjid, isowner, isMe, pushname, botNumber, senderNumber, sender, isGroup, args, isCmd, command, body, quoted }) => {
+        try {
+
+            if (!isSUB) return
+
+            conn.sendMessage(from, { react: { text: config.RTYPE3, key: mek.key } })
+
+            for (let i = 0; i < sendjid.length; i++) {
+
+                await conn.sendMessage(sendjid[i], {
+                    caption: "Jungle Cruise | 2021 | English | Movie | Sinhala Subtitles | TV Zone | " + config.FOOTER,
+                    footer: config.FOOTER,
+                    image: { url: "https://telegra.ph/file/0ac2f36a0a4d90f014cbb.jpg" }
+                })
+
+                conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+                await conn.sendMessage(sendjid[i], {
+                    document: { url: config.DBLINK + config.ENMVLINK + 'Jungle_Cruise_2021_@nadithpro.mkv' },
+                    mimetype: config.MKVTYPE,
+                    fileName: '@nadithpro Jungle Cruise 2021 .mkv'
+                })
+            } conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+
+        } catch (e) {
+            reply('ERROR !!')
+            console.log(e)
+        }
+    })
