6c397f80c6d937f86f16a78d98a89f23a3bd683a
Nadith Dhanula <116380737+nadith-pro@users.noreply.github.com>
2023-05-03 18:31:23 +0530

    Create Max Steel .js

---
+++ plugins/Movies/English/Max Steel .js
@@ -0,0 +1,35 @@
+const { cmd, commands } = require('../../../commands')
+
+cmd({
+    pattern: "maxsteel",
+    filename: __filename
+},
+    async (conn, mek, q, { reply, config, isSUB, from, sendjid, isowner, isMe, pushname, botNumber, senderNumber, sender, isGroup, args, isCmd, command, body, quoted }) => {
+        try {
+
+            if (!isSUB) return
+
+            conn.sendMessage(from, { react: { text: config.RTYPE3, key: mek.key } })
+
+            for (let i = 0; i < sendjid.length; i++) {
+
+                await conn.sendMessage(sendjid[i], {
+                    caption: "Max Steel | 2016 | English | Movie | Sinhala Subtitles | TV Zone | " + config.FOOTER,
+                    footer: config.FOOTER,
+                    image: { url: "https://telegra.ph/file/a5310ed2a86a870ef78fe.jpg" }
+                })
+
+                conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+                await conn.sendMessage(sendjid[i], {
+                    document: { url: config.DBLINK + config.ENMVLINK + 'Max_Steel_2016_@nadithpro.mkv' },
+                    mimetype: config.MKVTYPE,
+                    fileName: '@nadithpro Max Steel 2016 .mkv'
+                })
+            } conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+
+        } catch (e) {
+            reply('ERROR !!')
+            console.log(e)
+        }
+    })
