6206b4202bcfc6fec542c582cfe0839293e54c81
Nadith Dhanula <116380737+nadith-pro@users.noreply.github.com>
2023-05-05 11:29:41 +0530

    Update pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -4,37 +4,37 @@ module.exports = {
             "name": "tvzone-01",
             "script": "./index1.js",
             "instances": 1,
-            "exec_mode": "cluster"
+            "exec_mode": "fork"
         }, {
             "name": "tvzone-02",
             "script": "./index2.js",
             "instances": 1,
-            "exec_mode": "cluster"
+            "exec_mode": "fork"
         },{
             "name": "tvzone-03",
             "script": "./index3.js",
             "instances": 1,
-            "exec_mode": "cluster"
+            "exec_mode": "fork"
         },{
             "name": "tvzone-04",
             "script": "./index4.js",
             "instances": 1,
-            "exec_mode": "cluster"
+            "exec_mode": "fork"
         },{
             "name": "tvzone-05",
             "script": "./index5.js",
             "instances": 1,
-            "exec_mode": "cluster"
+            "exec_mode": "fork"
         },{
             "name": "tvzone-06",
             "script": "./index6.js",
             "instances": 1,
-            "exec_mode": "cluster"
+            "exec_mode": "fork"
         },{
             "name": "tvzone-07",
             "script": "./index7.js",
             "instances": 1,
-            "exec_mode": "cluster"
+            "exec_mode": "fork"
         }
     ]
 }
